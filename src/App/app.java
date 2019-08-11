8f9dd41fc6f09092c36e9643d710a9d110251483
ntd2471999 <nguyenthanhduy247199@gmail.com>
2019-08-11 20:55:29 +0700

    This my project restaurant management ui

--- src/App/app.java
+++ src/App/app.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -26,7 +27,8 @@ public class app extends Application {
         
         stage.setScene(scene);
         stage.initStyle(StageStyle.UNDECORATED);
-        
+         Image icon = new Image("/img/Logo.png");
+                stage.getIcons().add(icon);
         
         stage.show();
     }
